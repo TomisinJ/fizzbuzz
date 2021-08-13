73fb98d9c0ac62d9650156a14658f456c61afad1
tj <tomisinjoseph3@gmail.com>
2021-08-13 12:18:34 +0100

    Final commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FizzBuzz
+
+## Purpose
+
+- To create tests using rspec
+- To get comfortable using git
+- To build a simple program from the ground up
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,16 @@
 require './library/fizzbuzz'
 describe "fizzbuzz" do
-  it "if 3 returns fizz" do
+  it "if multiple of 3 returns fizz" do
     expect(fizzbuzz(3)).to eq("fizz")
+    expect(fizzbuzz(6)).to eq("fizz")
   end
-  it "if 5 returns buzz" do
+  it "if multiple of 5 returns buzz" do
     expect(fizzbuzz(5)).to eq("buzz")
+    expect(fizzbuzz(10)).to eq("buzz")
   end
-  it "if 15 returns fizzbuzz" do
+  it "if multiple of 15 returns fizzbuzz" do
     expect(fizzbuzz(15)).to eq("fizzbuzz")
+    expect(fizzbuzz(30)).to eq("fizzbuzz")
   end
   it "if 7 returns 7" do
     expect(fizzbuzz(7)).to eq(7)
