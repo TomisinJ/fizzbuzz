0c3b2c4e9b442148eb7c885fc8ffeaaa1d82bea9
tj <tomisinjoseph3@gmail.com>
2021-08-13 11:58:31 +0100

    fizzbuzz tests

--- library/fizzbuzz.rb
+++ library/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  "fizz"
+  if number == 3 
+    return "fizz"
+  elsif number == 5
+    return "buzz"
+  elsif number == 15
+    return "fizzbuzz"
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe "fizzbuzz" do
   it "if 3 returns fizz" do
     expect(fizzbuzz(3)).to eq("fizz")
   end
+  it "if 5 returns buzz" do
+    expect(fizzbuzz(5)).to eq("buzz")
+  end
+  it "if 15 returns fizzbuzz" do
+    expect(fizzbuzz(15)).to eq("fizzbuzz")
+  end
 end
